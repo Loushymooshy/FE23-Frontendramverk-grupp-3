a968e2af5656fd28f7c84e0fb9d3e6faf481b3cf
Loushymooshy <ellika.larsson2@gmail.com>
2024-04-26 11:36:49 +0200

    Component: Card stacking function

--- src/components/CardStack.jsx
+++ src/components/CardStack.jsx
@@ -1,8 +1,20 @@
 
-const CardStack = () => {
-  return (
-    <div>CardStack</div>
-  )
-}
+// const CardStack = () => {
 
-export default CardStack
+// const cardData = [
+//     {id: 1, namn: 'Kort 1', nummer: '1234 5678 9101 1121', utgångsdatum: '12/24', cvc: '123'},
+//     { id: 2, namn: 'Kort 2', nummer: '2345 6789 1011 1213', utgångsdatum: '11/25', cvc: '234' },
+//     { id: 3, namn: 'Kort 3', nummer: '3456 7890 1121 1314', utgångsdatum: '10/26', cvc: '345' }
+// ];
+
+
+//   return (
+//     <div>
+//         {cardData.map(card => (
+//             <Card key={card.id} data={card} />
+//         ))}
+//     </div>
+//   )
+// }
+
+// export default CardStack 
