48c52b5db3b4fe4743fc7b1ef497eeecd4acf2ae
Loushymooshy <ellika.larsson2@gmail.com>
2024-04-29 12:00:11 +0200

    css fix

--- src/pages/AddCardPage.jsx
+++ src/pages/AddCardPage.jsx
@@ -1,9 +1,10 @@
 import CardForm from '../components/CardForm.jsx';
 import Card from '../components/Card.jsx'
+import '../index.css'
 
 const AddCardPage = () => {
   return (
-    <div>
+    <div className='body'>
     <div>ADD A NEW BANK CARD</div>
     <Card
         cardNumber="1234 5678 9101 1121"
