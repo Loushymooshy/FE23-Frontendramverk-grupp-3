4d5137c0d0de34eb0d74f2aabc08cd030029f815
Loushymooshy <ellika.larsson2@gmail.com>
2024-04-29 11:42:39 +0200

    Card bug fix

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,4 @@
-import './Card.css';
+import "../styles/Card.css";
 
 import bitcoinLogo from '../assets/Bitcoin-logo.jpg';
 import chipLogo from '../assets/chip.png';
--- src/pages/AddCardPage.jsx
+++ src/pages/AddCardPage.jsx
@@ -1,16 +1,17 @@
 import CardForm from '../components/CardForm.jsx';
-
+import Card from '../components/Card.jsx'
 
 const AddCardPage = () => {
   return (
     <div>
     <div>ADD A NEW BANK CARD</div>
-    <CardForm></CardForm>
     <Card
         cardNumber="1234 5678 9101 1121"
         cardHolder="FIRSTNAME LASTNAME"
         expiryDate="MM/YY"
       />
+    <CardForm></CardForm>
+    
     </div>
   )
 }
