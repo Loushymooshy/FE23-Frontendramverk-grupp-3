a9a54a87bd248fe680d6fa4a95c5942f81fb5ff7
Lina Persson Signell <linasignell@live.se>
2024-04-29 18:06:45 +0200

    added style

--- src/components/CardForm.jsx
+++ src/components/CardForm.jsx
@@ -1,82 +1,107 @@
-import {useState} from 'react';
-import "../styles/CardForm.css";
+import { useState } from 'react';
+import '../styles/CardForm.css';
 import Card from './Card';
 const CardForm = () => {
+  const [cardData, setCardData] = useState({
+    cardNumber: '',
+    cardHolder: '',
+    expiryDate: '',
+    cvv: '',
+    vendor: '',
+  });
 
-    const [cardData, setCardData] = useState({
-        cardNumber: '',
-        cardHolder: '',
-        expiryDate: '',
-        cvv: '',
-        vendor: ''
-    })
+  const handleInputChange = e => {
+    const { id, value } = e.target;
+    setCardData(prevState => ({
+      ...prevState,
+      [id]: value,
+    }));
+  };
 
-    const handleInputChange = (e) => {
-        const { id, value} = e.target;
-        setCardData(prevState => ({
-            ...prevState,
-            [id]: value
-        }));
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    const newCard = {
+      id: Math.random(),
+      namn: cardData.cardHolder,
+      nummer: cardData.cardNumber,
+      utgångsdatum: cardData.expiryDate,
+      cvc: cardData.cvv,
     };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+    props.addCard(newCard);
+  };
 
-        const newCard = {
-            id: Math.random(), 
-            namn: cardData.cardHolder,
-            nummer: cardData.cardNumber, 
-            utgångsdatum: cardData.expiryDate,
-            cvc: cardData.cvv
-        };
-
-        props.addCard(newCard);
-    };
-
-
-    return (
+  return (
     <>
-      <Card 
-      cardNumber={cardData.cardNumber}
-      cardHolder={cardData.cardHolder}
-      expiryDate={cardData.expiryDate}
-    />
-    <form className="form-container">
+      <Card
+        cardNumber={cardData.cardNumber}
+        cardHolder={cardData.cardHolder}
+        expiryDate={cardData.expiryDate}
+      />
+      <form className="form-container">
         <div>
-            <label htmlFor="cardNumber">Kortnummer:</label>
-            <input className="form-input" id="cardNumber" type="text" onChange={handleInputChange} placeholder="1234 5678 9123 4567" />
+          <label htmlFor="cardNumber">Kortnummer:</label>
+          <input
+            className="form-input"
+            id="cardNumber"
+            type="text"
+            onChange={handleInputChange}
+            placeholder="1234 5678 9123 4567"
+          />
         </div>
 
         <div>
-            <label htmlFor="cardHolder">Kortinnehavare:</label>
-            <input className="form-input" id="cardHolder" type="text" onChange={handleInputChange} placeholder="Namn Efternamn" />
+          <label htmlFor="cardHolder">Kortinnehavare:</label>
+          <input
+            className="form-input"
+            id="cardHolder"
+            type="text"
+            onChange={handleInputChange}
+            placeholder="Namn Efternamn"
+          />
         </div>
 
-    <div className="half-width">
-        <div className="input-half">
+        <div className="half-width">
+          <div className="input-half">
             <label htmlFor="expiryDate">Utgångsdatum:</label>
-            <input className="form-input" id="expiryDate" type="text" onChange={handleInputChange}  placeholder="MM/ÅÅ" />
-        </div>
+            <input
+              className="form-input"
+              id="expiryDate"
+              type="text"
+              onChange={handleInputChange}
+              placeholder="MM/ÅÅ"
+            />
+          </div>
 
-        <div className="input-half">
+          <div className="input-half">
             <label htmlFor="cvv">CVV:</label>
-            <input className="form-input" id="cvv" type="text" onChange={handleInputChange} placeholder="123" />
+            <input
+              className="form-input"
+              id="cvv"
+              type="text"
+              onChange={handleInputChange}
+              placeholder="123"
+            />
+          </div>
         </div>
-     </div>
         <div>
-        <label htmlFor="vendor">Kortutgivare:</label>
-        <select className="form-input" id="vendor" onChange={handleInputChange}>
+          <label htmlFor="vendor">Kortutgivare:</label>
+          <select
+            className="form-input"
+            id="vendor"
+            onChange={handleInputChange}
+          >
             <option value="" disabled selected></option>
             <option value="bitcoin_inc">Bitcoin Inc</option>
             <option value="ninja_bank">Ninja Bank</option>
             <option value="block_chain_inc">Block Chain Inc</option>
             <option value="evil_corp">Evil Corp</option>
-        </select>
+          </select>
         </div>
-        
-    </form>
+      </form>
     </>
-    );
+  );
 };
 
 export default CardForm;
--- src/pages/AddCardPage.jsx
+++ src/pages/AddCardPage.jsx
@@ -2,49 +2,65 @@ import CardForm from '../components/CardForm.jsx';
 import Card from '../components/Card.jsx';
 import '../index.css';
 import Walletbutton from '../components/button.jsx';
-import {useState} from 'react';
+import { useState } from 'react';
 
 const AddCardPage = () => {
-
-
   const [cards, setCards] = useState([
-    {id: 1, namn: 'Kort 1', nummer: '1234 5678 9101 1121', utgångsdatum: '12/24', cvc: '123'},
-    {id: 2, namn: 'Kort 2', nummer: '2345 6789 1011 1213', utgångsdatum: '11/25', cvc: '234'},
-    {id: 3, namn: 'Kort 3', nummer: '3456 7890 1121 1314', utgångsdatum: '10/26', cvc: '345'}
+    {
+      id: 1,
+      namn: 'Kort 1',
+      nummer: '1234 5678 9101 1121',
+      utgångsdatum: '12/24',
+      cvc: '123',
+    },
+    {
+      id: 2,
+      namn: 'Kort 2',
+      nummer: '2345 6789 1011 1213',
+      utgångsdatum: '11/25',
+      cvc: '234',
+    },
+    {
+      id: 3,
+      namn: 'Kort 3',
+      nummer: '3456 7890 1121 1314',
+      utgångsdatum: '10/26',
+      cvc: '345',
+    },
   ]);
 
   const addCard = newCard => {
     setCards(prevCards => [...prevCards, newCard]);
-  }
-
+  };
 
   const handleClick = () => {
     //     console.log('knappen klickades');
-
   };
   return (
-    <div className="body">
-      <div>ADD A NEW BANK CARD</div>
+    <div className="container">
+      <div className="header">
+        ADD A NEW<br></br>BANK CARD
+      </div>
       {/* <Card
         cardNumber="1234 5678 9101 1121"
         cardHolder="FIRSTNAME LASTNAME"
         expiryDate="MM/YY"
       /> */}
-      <CardForm addCard={addCard}/>
+      <CardForm addCard={addCard} />
       <div>
-             {/* <Walletbutton
+        {/* <Walletbutton
           onClick={handleClick}
           text="LÄGG TILL KORT"
           className="button1" */}
         {/* /> */}
-         <Walletbutton
+        <Walletbutton
           onClick={handleClick}
           text="LÄGG TILL KORT"
           className="button2"
         />
-    </div>
+      </div>
     </div>
   );
 };
 
-export default AddCardPage
+export default AddCardPage;
