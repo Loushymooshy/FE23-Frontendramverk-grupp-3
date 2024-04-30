3a20058d24d32f083188090dd502ee592c288a35
Loushymooshy <ellika.larsson2@gmail.com>
2024-04-30 10:53:40 +0200

    Added vendor to Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,15 +3,17 @@ import '../styles/Card.css';
 import bitcoinLogo from '../assets/Bitcoin-logo.jpg';
 import chipLogo from '../assets/chip.png';
 
-function Card({ className, cardNumber, cardHolder, expiryDate }) {
+function Card({ className, cardNumber, cardHolder, expiryDate, vendor }) {
+  const { color, logo } = vendorStyles[vendor] || {color: "defaultColor", logo: chipLogo};
+
   return (
-    <div className={`card-container ${className}`}>
+    <div className={`card-container ${className}` style={{ backgroundColor: color }}>
       <div className="card-top">
         <div className="card-chip-logo">
           <img src={chipLogo} alt="Card Chip Logo" />
         </div>
         <div className="card-logo">
-          <img src={bitcoinLogo} alt="Bitcoin Logo" />
+          <img src={logo} alt="Bitcoin Logo" />
         </div>
       </div>
       <div className="card-number">{cardNumber || 'xxxx xxxx xxxx xxxx'}</div>
