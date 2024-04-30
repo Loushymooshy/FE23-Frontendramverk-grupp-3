f2b7159869bee22fbb6680623ecc206c2d36196a
Lina Persson Signell <linasignell@live.se>
2024-04-30 13:31:56 +0200

    css on main card homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,34 +8,31 @@ const HomePage = () => {
   const navigate = useNavigate();
   //navigate('/addcard');
 
-const jsonCards = window.localStorage.getItem('cards');
+  const jsonCards = window.localStorage.getItem('cards');
   const cards = jsonCards === null ? [] : JSON.parse(jsonCards);
-  console.log("Loaded cards:", cards);
+  console.log('Loaded cards:', cards);
 
   const handleClick = () => {
     navigate('/addcard');
-
-    
   };
 
   return (
     <div className="container">
       <div className="header">E-PLÅNBOK</div>
       <div>
-        <Card></Card>
+        <Card className={'main'}></Card>
       </div>
 
-      {
-      cards.map(card => 
-        (<Card
-           key={card.id} 
-            cardNumber={card.nummer}
-            cardHolder={card.namn}
-            expiryDate={card.utgångsdatum}
-            cvc={card.cvc}
-            vendor={card.vendor}
-          />)
-      )}
+      {cards.map(card => (
+        <Card
+          key={card.id}
+          cardNumber={card.nummer}
+          cardHolder={card.namn}
+          expiryDate={card.utgångsdatum}
+          cvc={card.cvc}
+          vendor={card.vendor}
+        />
+      ))}
       <div>
         <Walletbutton
           onClick={handleClick}
