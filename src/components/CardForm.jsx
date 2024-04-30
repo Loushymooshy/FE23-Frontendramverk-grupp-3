9c77458c2da53f5fb69e2eb74df98db47632c51b
Lina Persson Signell <linasignell@live.se>
2024-04-30 10:15:03 +0200

    button fix

--- src/components/CardForm.jsx
+++ src/components/CardForm.jsx
@@ -41,8 +41,9 @@ const CardForm = () => {
     props.addCard(newCard);
   };
 
+  const navigate = useNavigate();
+
   const handleClick = () => {
-    const navigate = useNavigate();
     navigate('/');
   };
 
