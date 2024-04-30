dc5ae41d824f843ca815d4fdb191b66579c93091
Rebecca <rebeccaaliina@gmail.com>
2024-04-30 23:14:32 +0200

    rensning
    rensning

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AddCardPage from './pages/AddCardPage';
